e417ed1deac5942add981e2a9a155df1c017f4fe
Matt Dittes <mattdittes@gmail.com>
2022-09-07 17:37:35 -0400

    commit tree 9e0cc372153c4783696cb9199f6af811d10ad97c

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,22 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        #self.freebies.where(item_name: item_name) != []
+        #self.freebies.exists?(item_name: item_name)
+        end
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev: dev) unless freebie.dev != self
+        #if freebie.dev_id == self.id
+            #freebie.update(dev_id: dev.id)
+        #else
+            #false
+        #end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,14 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "shirt", value: 25, company_id: c1.id, dev_id: d1.id)
+Freebie.create(item_name: "mug", value: 15, company_id: c3.id, dev_id: d2.id)
+Freebie.create(item_name: "pen", value: 5, company_id: c2.id, dev_id: d2.id)
+Freebie.create(item_name: "journal", value: 10, company_id: c4.id, dev_id: d3.id)
+Freebie.create(item_name: "umbrella", value: 18, company_id: c1.id, dev_id: d4.id)
+Freebie.create(item_name: "sweatshirt", value: 38, company_id: c1.id, dev_id: d3.id)
+Freebie.create(item_name: "tote bag", value: 21, company_id: c2.id, dev_id: d1.id)
+Freebie.create(item_name: "lanyard", value: 5, company_id: c3.id, dev_id: d4.id)
+Freebie.create(item_name: "sunglasses", value: 12, company_id: c4.id, dev_id: d2.id)
 
 puts "Seeding done!"
